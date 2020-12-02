1ce79d17a0bc4dba9f5112c828794a973cddf4ed
andersinde <andersinde@gmail.com>
2020-12-02 17:24:34 +0100

    clean up

--- animation.py
+++ animation.py
@@ -18,24 +18,19 @@ def makeanimation():
     from cv2 import VideoWriter, VideoWriter_fourcc
     import glob
 
-    (height, width) = np.shape(getMatrixFromPPM("frames/0.ppm"))
+    # get width, heigth, frames
+    f = open(filename, "./frames/0.ppm")
+    line0 = f.readline()
+    line1 = f.readline().split(" ")
+    width, height  = int(line1[0]), int(line1[1])
     frames = len(glob.glob("./frames/*.ppm"))
     FPS = 60
 
-    frame = np.zeros((height, width, 3), dtype=np.uint8) #black frame
-    #frame = np.full((height, width, 3), 255, dtype=np.uint8) #white frame
-
     fourcc = VideoWriter_fourcc(*'mp4v')
     video = VideoWriter('./fractal.mp4', fourcc, float(FPS), (width, height))
-
+    frame = np.zeros((height, width, 3), dtype=np.uint8)
 
     for i in range(frames):
-        matrix = getMatrixFromPPM(f"./frames/{i}.ppm")
-        np.size(height)
-        #for x in range(height):
-            #for y in range(width):
-                #c = matrix[x][y]
-                #frame[x, y] = [c, c, c]
         f = open(f"./frames/{i}.ppm", "r")
         line0 = f.readline()
         line1 = f.readline()
